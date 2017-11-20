9929c5586417653b7a4445184deb92891b643281
Alexander Aguilar <alexanderman2004@yahoo.com>
2017-11-19 22:24:05 -0500

    master now up to date

--- dist/js/GameModel.js
+++ dist/js/GameModel.js
@@ -1,148 +1,114 @@
-const MidiConvert       = require('./lib/MidiConvert');
 const Tone              = require('./lib/Tone');
 
 class GameModel {
     constructor() {
-        this.isPlaying = false;     // track if music playing
-        this.allParts = [];         // track all Tone parts
-        this.selectedNotes = [];    // measures selected to be played
-        this.theScore = null;       // available measures to choose from
-        this.synth = null;          // instrument used to play song
+        this.isPlaying = false;         // track if music playing
+        this.allEvents = [];            // events for playing .wavs
+        this.selectedNotes = [];        // measures selected to be played
+        this.notePaths = [];            // paths to selected notes
+        this.theScore = null;           // available measures to choose from
 
         this.init();
     }
 
     init() {
-        this.createSynth();
+        // default instrument to play
+        this.selectedPath = './audio/acoustic_grand_piano/';
         this.createScore();
     }
 
     // forms base table for theScore
+    // TODO: simplify this
     createScore()
     {
         this.theScore = [
-            {measures: ["M96.MID", "M32.MID", "M69.MID", "M40.MID",
-                "M148.MID", "M104.MID", "M152.MID", "M119.MID",
-                "M98.MID", "M3.MID", "M54.MID"]},
-            {measures: ["M22.MID", "M6.MID", "M95.MID", "M17.MID",
-                "M74.MID", "M157.MID", "M60.MID", "M84.MID",
-                "M142.MID", "M87.MID", "M130.MID"]},
-            {measures: ["M141.MID", "M128.MID", "M158.MID", "M113.MID",
-                "M163.MID", "M27.MID", "M171.MID", "M114.MID",
-                "M42.MID", "M165.MID", "M10.MID"]},
-            {measures: ["M41.MID", "M63.MID", "M13.MID", "M85.MID",
-                "M45.MID", "M167.MID", "M53.MID", "M50.MID",
-                "M156.MID", "M61.MID", "M103.MID"]},
-            {measures: ["M105.MID", "M146.MID", "M153.MID", "M161.MID",
-                "M80.MID", "M154.MID", "M99.MID", "M140.MID",
-                "M75.MID", "M135.MID", "M28.MID"]},
-            {measures: ["M122.MID", "M46.MID", "M55.MID", "M2.MID",
-                "M97.MID", "M68.MID", "M133.MID", "M86.MID",
-                "M129.MID", "M47.MID", "M37.MID"]},
-            {measures: ["M11.MID", "M134.MID", "M110.MID", "M159.MID",
-                "M36.MID", "M118.MID", "M21.MID", "M169.MID",
-                "M62.MID", "M147.MID", "M106.MID"]},
-            {measures: ["M30.MID", "M81.MID", "M24.MID", "M100.MID",
-                "M107.MID", "M91.MID", "M127.MID", "M94.MID",
-                "M123.MID", "M33.MID", "M5.MID"]},
-            {measures: ["M70.MID", "M117.MID", "M66.MID", "M90.MID",
-                "M25.MID", "M138.MID", "M16.MID", "M120.MID",
-                "M65.MID", "M102.MID", "M35.MID"]},
-            {measures: ["M121.MID", "M39.MID", "M139.MID", "M176.MID",
-                "M143.MID", "M71.MID", "M155.MID", "M88.MID",
-                "M77.MID", "M4.MID", "M20.MID"]},
-            {measures: ["M26.MID", "M126.MID", "M15.MID", "M7.MID",
-                "M64.MID", "M150.MID", "M57.MID", "M48.MID",
-                "M19.MID", "M31.MID", "M108.MID"]},
-            {measures: ["M9.MID", "M56.MID", "M132.MID", "M34.MID",
-                "M125.MID", "M29.MID", "M175.MID", "M166.MID",
-                "M82.MID", "M164.MID", "M92.MID"]},
-            {measures: ["M112.MID", "M174.MID", "M73.MID", "M67.MID",
-                "M76.MID", "M101.MID", "M43.MID", "M51.MID",
-                "M137.MID", "M144.MID", "M12.MID"]},
-            {measures: ["M49.MID", "M18.MID", "M58.MID", "M160.MID",
-                "M136.MID", "M162.MID", "M168.MID", "M115.MID",
-                "M38.MID", "M59.MID", "M124.MID"]},
-            {measures: ["M109.MID", "M116.MID", "M145.MID", "M52.MID",
-                "M1.MID", "M23.MID", "M89.MID", "M72.MID",
-                "M149.MID", "M173.MID", "M44.MID"]},
-            {measures: ["M14.MID", "M83.MID", "M79.MID", "M170.MID",
-                "M93.MID", "M151.MID", "M172.MID", "M111.MID",
-                "M8.MID", "M78.MID", "M131.MID"]},
+            {measures:
+                [ "M96",  "M32",  "M69",  "M40", "M148", "M104", "M152", "M119",  "M98",   "M3",  "M54"]},
+            {measures:
+                [ "M22",   "M6",  "M95",  "M17",  "M74", "M157",  "M60",  "M84", "M142",  "M87", "M130"]},
+            {measures:
+                ["M141", "M128", "M158", "M113", "M163",  "M27", "M171", "M114",  "M42", "M165",  "M10"]},
+            {measures:
+                [ "M41",  "M63",  "M13",  "M85",  "M45", "M167",  "M53",  "M50", "M156",  "M61", "M103"]},
+            {measures:
+                ["M105", "M146", "M153", "M161",  "M80", "M154",  "M99", "M140",  "M75", "M135",  "M28"]},
+            {measures:
+                ["M122",  "M46",  "M55",   "M2",  "M97",  "M68", "M133",  "M86", "M129",  "M47",  "M37"]},
+            {measures:
+                [ "M11", "M134", "M110", "M159",  "M36", "M118",  "M21", "M169",  "M62", "M147", "M106"]},
+            {measures:
+                [ "M30",  "M81",  "M24", "M100", "M107",  "M91", "M127",  "M94", "M123",  "M33",   "M5"]},
+            {measures:
+                [ "M70", "M117",  "M66",  "M90",  "M25", "M138",  "M16", "M120",  "M65", "M102",  "M35"]},
+            {measures:
+                ["M121",  "M39", "M139", "M176", "M143",  "M71", "M155",  "M88",  "M77",   "M4",  "M20"]},
+            {measures:
+                [ "M26", "M126",  "M15",   "M7",  "M64", "M150",  "M57",  "M48",  "M19",  "M31", "M108"]},
+            {measures:
+                [  "M9",  "M56", "M132",  "M34", "M125",  "M29", "M175", "M166",  "M82", "M164",  "M92"]},
+            {measures:
+                ["M112", "M174",  "M73",  "M67",  "M76", "M101",  "M43",  "M51", "M137", "M144",  "M12"]},
+            {measures:
+                [ "M49",  "M18",  "M58", "M160", "M136", "M162", "M168", "M115",  "M38",  "M59", "M124"]},
+            {measures:
+                ["M109", "M116", "M145",  "M52",   "M1",  "M23",  "M89",  "M72", "M149", "M173",  "M44"]},
+            {measures:
+                [ "M14",  "M83",  "M79", "M170",  "M93", "M151", "M172", "M111",   "M8",  "M78", "M131"]}
         ];
-    }
-
-    // create the instrument for playing the song
-    createSynth() {
-        this.synth = new Tone.PolySynth(8, Tone.Synth, {
-            "oscillator": {
-                "type": "sine3"
-            },
-            "envelope": {
-                "attack": 0.03,
-                "decay": 0.1,
-                "sustain": 0.2,
-                "release": 0.6
-            }
-        }).toMaster();
     }
 
     // return random measure from an array
     randMeasure(noteArray) {
-        var num = Math.floor(Math.random() * noteArray.length);
+        let num = Math.floor(Math.random() * noteArray.length);
         return noteArray[num];
     }
 
     // creates a random song
     randomSong() {
-        var selectedNotes = [];
+        let selectedNotes = [];
+        let notePaths = [];
 
-        for (var i = 0; i < this.theScore.length; i++) {
-            selectedNotes.push(this.randMeasure(this.theScore[i].measures));
+        for (let i = 0; i < this.theScore.length; i++) {
+            let name = this.randMeasure(this.theScore[i].measures);
+
+            selectedNotes.push(name);
+            notePaths.push(this.selectedPath + name + '.wav');
         }
 
         this.selectedNotes = selectedNotes;
-    }
-
-    // play a single note
-    playNote(time, event, synth) {
-        this.synth.triggerAttackRelease(event.name,
-            event.duration,
-            time,
-            event.velocity);
+        this.notePaths = notePaths;
     }
 
     // load the entirety of the selectedNotes
     loadSong() {
-        var offset = 0;
+        let minuets = new Tone.Buffers(this.notePaths, function() {
+            // offset for each
+            let offset = 0;
 
-        for (var i = 0; i < this.selectedNotes.length; i++) {
-            // load in each midi file for playing
-            MidiConvert.load("./audio/mozartMidi/" + this.selectedNotes[i]).then(function(midi) {
-                Tone.Transport.bpm.value = midi.bpm; // remove?
-                var theNotes = midi.tracks[0].notes;
-                var aPart = new Tone.Part(function(time, note) {
-                    this.playNote(time, note, this.synth);
-                }.bind(this), theNotes).start(offset);
+            // loop through all minuets
+            for (let i = 0; i < this.notePaths.length; i++) {
+                // get current buffer
+                let buf = minuets.get(i);
 
-                // testing parts
-                this.allParts.push(aPart);
+                // create an event for it
+                let evt = new Tone.Event(function(time, song) {
+                    let player = new Tone.Player(song).toMaster();
+                    player.start();
+                }.bind(this), buf).start(offset);
 
-                // take last note and add to offset
-                var lastNote = theNotes.slice(-1)[0];
+                this.allEvents.push(evt);
 
-                // there appears to be a delay in the measures. using 1.5 as temp fix
-                offset += lastNote.time - 1.5;
-            }.bind(this));
-        }
+                offset += buf.duration - 2.0;
+            }
+        }.bind(this));
     }
 
     // method clears Tone of existing song
     clearSong() {
-        for (var part in this.allParts) {
-            this.allParts[part].removeAll();
+        for (let evt in this.allEvents) {
+            this.allEvents[evt].dispose();
         }
-        this.allParts = [];
+        this.allEvents = [];
     }
 }
 
